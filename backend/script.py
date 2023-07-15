f84628286b44708bb1bb9ad65a43297a17a6a91e
JulioHerculano <julyocesarplay@gmail.com>
2023-07-15 09:59:34 -0300

    adicionado espacamento nas linhas 4 e 5

--- backend/script.py
+++ backend/script.py
@@ -2,6 +2,7 @@ operacao = input("Digite a operacao desejada (soma, sub, mult, div): ")
 numero1 = input("Digite o primeiro número: ")
 numero2 = input("Digite o segundo número: ")
 resultado = 0
+
 if operacao == "soma":
 	resultado = int(numero1) + int(numero2)
 if operacao == "sub":
@@ -12,5 +13,5 @@ if operacao == "div":
 	resultado = int(numero1) / int(numero2)
 else:
 	resultado = "Operação não suportada"
-    
+	  
 print("O resultado da operação é: " , resultado)
